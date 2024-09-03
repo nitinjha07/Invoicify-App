c79d485c0e5e85b8bff965a1e871b6dc697f1e08
nitinjha07 <nitinkumarjha245@gmail.com>
2024-09-03 13:16:21 +0530

    Log out screen text changed

--- app/(tabs)/profile.jsx
+++ app/(tabs)/profile.jsx
@@ -17,36 +17,39 @@ const profile = () => {
     };
 
     return (
-        <SafeAreaView className="bg-primary h-full">
-            <View className="w-full flex justify-center items-center mt-6 mb-12 px-4 z-2">
-                <TouchableOpacity
-                    onPress={logout}
-                    className="absolute top-6 right-4 flex-row items-center bg-trans p-2 rounded-xl z-2" // Absolute positioning and flex row for alignment
-                >
-                    <Text className="text-secondary font-pbold">
-                        LogOut
-                    </Text>
-                    <Image
-                        source={icons.logout}
-                        resizeMode="contain"
-                        className="w-6 h-6 ml-2"
-                    />
+      <SafeAreaView className="bg-primary h-full">
+        <View className="w-full flex justify-center items-center mt-6 mb-12 px-4 z-2">
+          <TouchableOpacity
+            onPress={logout}
+            className="absolute top-6 right-4 flex-row items-center bg-trans p-2 rounded-xl z-2" // Absolute positioning and flex row for alignment
+          >
+            <Text className="text-secondary font-pbold">LogOut</Text>
+            <Image
+              source={icons.logout}
+              resizeMode="contain"
+              className="w-6 h-6 ml-2"
+            />
+          </TouchableOpacity>
+        </View>
 
-                </TouchableOpacity>
-            </View>
-
-            <View className="flex-1 mt-8 justify-center items-center bg-primary">
-                <View className="mt-2 w-32 h-32 rounded-full overflow-hidden border border-gray-300 mb-4">
-                    <Image
-                        source={{ uri: 'https://www.pngitem.com/pimgs/m/150-1503945_transparent-user-png-default-user-image-png-png.png' }} // Replace with your image source
-                        style={styles.profileImage}
-                    />
-                </View>
-                <Text className="text-lg font-pbold text-secondary">Akash Gupta</Text>
-                <Text className="text-lg font-bold text-secondary">akash.gupta.ug22@nsut.ac.in</Text>
-            </View>
-        </SafeAreaView>
-    )
+        <View className="flex-1 mt-8 justify-center items-center bg-primary">
+          <View className="mt-2 w-32 h-32 rounded-full overflow-hidden border border-gray-300 mb-4">
+            <Image
+              source={{
+                uri: "https://www.pngitem.com/pimgs/m/150-1503945_transparent-user-png-default-user-image-png-png.png",
+              }} // Replace with your image source
+              style={styles.profileImage}
+            />
+          </View>
+          <Text className="text-lg font-pbold text-secondary">
+            Your Profile
+          </Text>
+          <Text className="text-lg font-pbold text-secondary w-[70%] text-center mt-10">
+            Ready to leave? Click the button above to log out
+          </Text>
+        </View>
+      </SafeAreaView>
+    );
 };
 const styles = StyleSheet.create({
     profileImage: {
